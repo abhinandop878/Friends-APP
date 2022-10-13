51a8d7b113a8b49e3aeea8b3f4fcee8d9493dd2a
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-13 22:46:55 +0530

    ViewFriends

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ViewFriendsComponent } from './view-friends/view-friends.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ViewFriendsComponent
   ],
   imports: [
     BrowserModule,
